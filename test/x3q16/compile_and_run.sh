fedc5d258e7876be54d2b7a607fddbd5bb1759ac
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-08-01 10:12:30 -0400

    fix files

--- test/x3q16/compile_and_run.sh
+++ test/x3q16/compile_and_run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+#delete files if they exist
+if test -f program.bin; then
+	rm program.bin
+fi
+
+if test -f program_checks.tv; then
+	rm program_checks.tv
+fi
+
+#assemble program
+
+customasm $1 -f hexstr -o program.bin
+cp $2 program_checks.tv
+
+python3 splice.py
+
+#compile and run verilog
 iverilog x3q16_tb.v -I ../../src/
 ./a.out
